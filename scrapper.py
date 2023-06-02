671767d2f7afd576f69d55fec98b3fad4086749e
bas-clavijo <bas.clavijo@duocuc.cl>
2023-06-02 14:20:26 -0400

    Script para busqueda de trabajo

---
+++ scrapper.py
@@ -0,0 +1,35 @@
+# Script para busqueda de trabajo
+import requests
+from bs4 import BeautifulSoup #libreria para poder hacer web scrapper
+
+url = "https://www.seek.co.nz/python-jobs?salaryrange=100000-999999&salarytype=annual"
+
+
+if "__main__" == __name__:
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    def has_data_search(tag):
+        return tag.has_attr("data-search-sol-meta")
+    results = soup.find_all(has_data_search)
+
+    for job in results:
+        try:
+            titleElement = job.find("a", attrs={"data-automation": "jobTitle"})
+            title = titleElement.get_text()
+            company = job.find("a", attrs={"data-automation": "jobCompany"}).get_text()
+            joblink = "https://www.seek.co.nz" + titleElement["href"]
+            salary = job.find("span", attrs={"data-automation": "jobSalary"})
+            salary = salary.get_text() if salary else 'n/a'
+
+            job = "Titulo: {}\nEmpresa: {}\nSalario: {}\nLink: {}a\n"
+
+            job = job.format(title, company, salary, joblink)
+
+            print(job)
+        except Exception as e:
+            print("Exception: {}".format(e))
+            pass
+
+#py scrapper.py >> jobs.txt
+#creacion del archivo txt con la informacion detallada, esta informacion ira dependiendo del link y la busqueda actual
